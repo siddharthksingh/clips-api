3d5cc53dbd2d9de8c08e6941fd7fd046265fc87f
siddharthksingh <sid2373iitk@gmail.com>
2025-07-05 19:25:42 +0530

    Added POST /clips route to add new clips

--- app/main.py
+++ app/main.py
@@ -86,6 +86,5 @@ def create_clip(clip_data: ClipCreate, db: Session = Depends(get_db)):
     db.refresh(new_clip)
     return new_clip
 
-
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
